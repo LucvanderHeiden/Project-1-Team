9d39a97ab7255e5b4ef2341cbf55ca5e46578768
Luc van der Heiden <lucvdheiden@hotmail.com>
2023-05-11 12:41:17 +0200

    updated lvl 2.5 objectives

---
+++ Assets/ObjectiveUpdater.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ObjectiveUpdater : MonoBehaviour
+{
+    private Inventory inventory;
+    public TextMeshProUGUI currentObjectiveText;
+    public TextMeshProUGUI newObjectiveText;
+
+    public Image dialogueImage;
+    public TextMeshProUGUI dialogueText;
+    private bool openDialogue = true;
+
+    private void Start()
+    {
+        inventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+
+        dialogueImage.enabled = false;
+        dialogueText.gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (inventory.isFull[0] && inventory.isFull[1] && inventory.isFull[2] && inventory.isFull[3] && openDialogue)
+        {
+            currentObjectiveText.gameObject.SetActive(false);
+            newObjectiveText.gameObject.SetActive(true);
+
+            dialogueImage.enabled = true;
+            dialogueText.gameObject.SetActive(true);
+
+            openDialogue = false;
+
+            StartCoroutine(UpdateObjective());
+        }
+    }
+
+    private IEnumerator UpdateObjective()
+    {
+        WaitForSeconds wait = new WaitForSeconds(5f);
+        yield return wait;
+
+        dialogueImage.enabled = false;
+        dialogueText.gameObject.SetActive(false);
+    }
+}
